45140aa96495e4f038fbc744a47e57e90750f2c6
DuyCUongggg <duycuongpdc@gmail.com>
2025-09-11 13:22:49 +0700

    Delete PDC_app directory

--- PDC_app/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles();   // serve index.html khi truy cập /
-app.UseStaticFiles();    // cho phép tải /css, /js, /data
-
-app.MapFallbackToFile("index.html");
-app.Run();
